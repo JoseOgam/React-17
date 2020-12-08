285def3d653479b836ae948e6717a4796356eafb
JoseOgam <jotienoochieng@gmail.com>
2020-12-08 06:13:33 -0500

    first commit

---
+++ src/components/AddTodo.js
@@ -0,0 +1,13 @@
+const AddTodos = () => {
+    return (
+        <div>
+            <form>
+                <input placeholder='title' />
+                <textarea placeholder='body' />
+                <button>Add Todos</button>
+            </form>
+        </div>
+    )
+ }
+
+export default AddTodos
