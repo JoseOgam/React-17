f475e12b2a99c59b94f44fdcc65426d38676ed5e
JoseOgam <jotienoochieng@gmail.com>
2020-12-27 13:57:28 -0500

    installed necessary libraries for the project

---
+++ mimick/index.js
@@ -0,0 +1,24 @@
+// const add = (a, b) => {
+//     if (Object.is)
+//     {
+//         return `number are equal try other numbers`;
+//     }
+//     else
+//     {
+//         return `numbers are not equal try other numbers`;
+//     };
+// }
+// console.log(add(5, 15))
+    
+function even() {
+    
+for (let i = 0; i <= 10; i++)
+{
+    if (i % 2 !== 0)
+    {
+     console.log(i) 
+    }
+    
+}
+}
+console.log(even()) 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
+import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(
   <React.StrictMode>
